64e4707fd52d22934a2d770938a81d3e3d4e4d4f
Edison Orellana <me@edisonorellana.com>
2016-06-20 17:20:50 +0000

    got bimodtest working, now th minimum

---
+++ th_minimum.R
@@ -0,0 +1,13 @@
+mum <- function(I, n = 255) {
+    # Find a global threshold for a grayscale image by choosing the threshold to
+    # be in the valley of the bimodal histogram. The method is also known as the
+    # mode method.
+    
+    # In:
+    # I : Grayscale image
+    # n : maximum graylevel (default 255)
+    
+    
+    # Copyright (C) 2004-2013 Antti Niemist
+    # This file is part of HistThresh toolbox
+}
