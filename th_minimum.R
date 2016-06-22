937dd0cccadc5314250cad6d5d5aec7629dc3ee9
Edison Orellana <me@edisonorellana.com>
2016-06-22 14:10:26 -0500

    fixed formatting

--- th_minimum.R
+++ th_minimum.R
@@ -1,41 +1,33 @@
 T <- function(I, n = 255) {
-    # Find a global threshold for a grayscale image by choosing the threshold to
-    # be in the valley of the bimodal histogram. The method is also known as the
-    # mode method.
-    
+    # Find a global threshold for a grayscale image by choosing the threshold
+    # to be in the valley of the bimodal histogram. The method is also known
+    # as the mode method.
     # In:
     # I : Grayscale image
-    # n : maximum graylevel (default 255)
-    
-    
-    # Copyright (C) 2004-2013 Antti Niemist
-    # This file is part of HistThresh toolbox
+    # n: maximum graylevel (default 255)
+
+    #---------------------------------------------------------------------#
+
+    source('./bimodtest.R')
+
+    # Load images
+    image <- load.image(file = I)
+    image <- B(image) # only blue band
+    image_histogram <- hist(x = image,
+                            breaks = n,
+                            col = 'blue',
+                            main = cat('Histogram of ', I))
+    # ???
 
     iter <- 0
-    while (bimodtest(y)) {
-        # do thing
+    while (bimodtest(image_histogram)) {
         h <- matrix(1, 3) / 3
-        y <- convolution(y, h, 'same')
-        iter += 1
-
-        # if the histogram turns out not to be bimodal, set T to zero
+        image_histogram <- convolution (image_histogram, h, 'same')
+        iter <- iter + 1
+        # If the histogram turns out not to be bimodal, set T to zero
         if (iter > 10000) {
             T <- 0
             return()
         }
-    }
-}
-
-peakfound <- FALSE
-
-for (k in 2:n) {
-    # do a thing
-    if ( y(k - 1) < y(k) & y(k + 1) < y(k)) {
-        #peak found
-        peakfound <- TRUE
-    }
-    if (peakfound & y(k-1) >= y(k) & y(k + 1) >= y(k) ) {
-        T <- k - 1
-        return()
     }
 }
