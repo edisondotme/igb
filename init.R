0387c157a49cda82022df8071180d4cdc3361451
Edison Orellana <me@edisonorellana.com>
2016-07-27 09:21:00 -0500

    deleted old unfoldered files

--- init.R
+++
@@ -1,7 +0,0 @@
-init <- function() {
-    library(imager)
-    image <- imager::load.image("C:\\Users\\me\\Documents\\igb\\cameraman.png")
-    image <- B(image)
-    image_histogram <- hist(x = image*255, breaks = 0:255)
-    return(image)
-}
